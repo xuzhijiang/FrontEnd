a7fba4af1848d835aadf059750ad15d13794c729
XuZhiJiang <xuzhijiang@sunniwell.net>
2018-06-12 17:24:56 +0800

    test

--- test/coreweb.py
+++ test/coreweb.py
@@ -1,28 +1,3 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import functools
-
-
-def get(path):
-    '''
-    Define decorator @get('/path')
-    '''
-    def decorator(func):
-        @functools.wraps(func)
-	def wrapper(*args, **kw)
-            func(*args, **kw)
-	return wrapper
-	wrapper.__method__ = 'GET'
-	wrapper.__route__ = path
-    return decorator
-
-def post(path):
-    def decorator(func):
-        @functools.wraps(func)
-	def wrapper(*args, **kw):
-	    func(*args, **kw)
-	return wrapper
-	wrapper.__method__ = 'POST'
-	wrapper.__route__ = path
-    return decorator
