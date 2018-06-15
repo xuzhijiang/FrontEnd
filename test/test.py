946f9a3e5ea99090314fb700f60111b50ba77803
XuZhiJiang <xuzhijiang@sunniwell.net>
2018-06-15 17:50:34 +0800

    add idea to gitignore

--- test/test.py
+++ test/test.py
@@ -2,21 +2,11 @@
 # -*- coding: utf-8 -*-
 
 
-def add_routes(module_name):
-    n = module_name.rfind('.')
-    print('n: %s' % n)
-    if n == (-1):
-        mod = __import__(module_name, globals(), locals())
-    else:
-        name = module_name[n+1:]
-        mod = __import__(module_name[:n], globals(), locals(), [name])
-    print(dir(mod))
-    for attr in dir(mod):
-        if attr.startswith('__'):
-            continue
-        fn = getattr(mod, attr)
+def init_jinja2(**kw):
+    options = dict(
+        autoescape = kw.get('autoescape', True),
+        auto_reload = kw.get('auto_reload', True)
+        )
+    print('options: %s' % options)
 
-
-
-add_routes("handlers.py")
-
+init_jinja2(filter=dict(autoescape=False))
