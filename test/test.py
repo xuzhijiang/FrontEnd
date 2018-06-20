5c3c49102772a800ca04482b3085d845565a134e
XuZhiJiang <xuzhijiang@sunniwell.net>
2018-06-20 18:53:17 +0800

    学习JavaScript

--- test/test.py
+++ test/test.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-class T():
-    pass
 
-d = T()
-d.b = 'B'
-print(d.b)
+def get_page_index(p_str):
+	page = 1
+	try:
+		page = int(p_str)
+	except ValueError as e:
+		print('error')
+		pass
+	print('page: %s' % page)
+	if page < 1:
+		page = 1
+	return page
+
+print(get_page_index('35'))
