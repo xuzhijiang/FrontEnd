510166bd759ec48af09c21a9b55aa6a3c2157c2f
XuZhiJiang <xuzhijiang@sunniwell.net>
2018-06-26 18:55:04 +0800

    commit

--- test/test.py
+++ test/test.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+from pymongo import MongoClient
 
-import logging, inspect, time, uuid
-logging.basicConfig(level=logging.INFO)
+client = MongoClient('mongodb://localhost:27017')
+db = client.pymongo_test
 
-def foo(x):
-	return x * x
-
-r = map(foo, [1, 2, 4, 8])
-print(list(r))
+posts = db.posts
+post_data = {
+	'title': 'Python and MongoDb',
+	'content': 'learn MongoDb in Python',
+	'author': 'xzj'
+}
+result = posts.insert_one(post_data)
+print('One post: {0}'.format(result.inserted_id))
